5696cb794a0fbf18efcf04845a1099ebe4ade9c1
KantaKawashima <72531787+KantaKawashima@users.noreply.github.com>
2020-10-15 16:02:22 +0900

    Create Task1_4.java
    Task_1-4の作成

---
+++ JavaStudy/1-4/Task1_4.java
@@ -0,0 +1,27 @@
+public class Task1_4 {
+
+    // 定数（アカウント情報）
+   String name = "alice";
+   String pass = "pass";
+    // 定数（メッセージ）
+    private static final String CONST_MSG_SUCCESS = "ログイン成功です。";
+    private static final String CONST_MSG_ERROR_NAME = "名前に誤りがあります。";
+    private static final String CONST_MSG_ERROR_PASS = "パスワードに誤りがあります。";
+    private static final String CONST_MSG_ERROR_INPUT = "入力情報に誤りがあります。";
+    public static void main(String[] args) {
+ 
+         String name = "alice";
+         String pass = "pass";
+  
+        if (name.equals("alice") && pass.equals("pass")){
+          System.out.println(CONST_MSG_SUCCESS) ;
+        }else if (name.equals("alice")){
+          System.out.println(CONST_MSG_ERROR_PASS) ;
+        }else if (pass.equals("pass")){
+          System.out.println(CONST_MSG_ERROR_NAME) ;
+        }else  {
+          System.out.println(CONST_MSG_ERROR_INPUT);
+        } 
+    }
+
+}
